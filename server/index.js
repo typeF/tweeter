99b4053404b2a6a91d85bca5b82efc1e55d7e876
Frank Lam <frank.is.missing@gmail.com>
2017-12-18 04:02:44 +0000

    Added sass functionality

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const path = require('path');
 const cookieSession = require('cookie-session');
+const sassMiddleware = require('node-sass-middleware');
 const bcrypt = require('bcrypt');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +14,12 @@ app.use(express.static("public"));
 app.use(cookieSession({
   name: 'session',
   keys: ['super top secret key']
+}));
+
+app.use(sassMiddleware({
+  src: __dirname + "/../public",
+  dest: path.join(__dirname, '/..', 'public'),
+  debug: true,
 }));
 
 const {MongoClient} = require("mongodb");
